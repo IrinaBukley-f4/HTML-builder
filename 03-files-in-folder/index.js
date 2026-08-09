d44395ad76672a0e85fa14287ea0860686dede47
Irina <irenserge@mail.ru>
2026-08-09 12:05:01 +0300

    feat: add 03-task-decision

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const pathDir = path.join(path.dirname(__filename), 'secret-folder');
+
+async function parseDir() {
+  try {
+    const elems = await fs.readdir(pathDir);
+    for( let elem of elems) {
+      const elemPath = path.join(pathDir, elem);  
+      const stats = await fs.stat(elemPath);
+      if(stats.isFile()) {
+        const elemName = path.parse(elem).name;
+        const elemExtention = path.extname(elem).slice(1);
+        const elemSize = stats.size / 1000;
+        console.log(`${elemName} - ${elemExtention} - ${elemSize}kb`);
+      }
+    }
+  } catch (error) {
+    return console.error(error.message);  
+  }
+}
+parseDir();
